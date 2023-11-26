9bad0589e5d57fb6fb276c61b0267d88e59f6764
xyzjgmday <daysubhiz@gmail.com>
2023-11-26 17:52:13 +0700

    Pertemuan-6

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 
 class PostsController extends Controller
@@ -13,23 +14,68 @@ class PostsController extends Controller
      * 
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $posts = Post::OrderBy("id", "DESC")->paginate(10);
+        $acceptHeader = $request->header('Accept');
 
-        $outPut = [
-            "message" => "posts",
-            "result" => $posts
-        ];
+        if ($acceptHeader === 'application/json' || $acceptHeader === 'application/xml') {
+            $posts = Post::OrderBy("id", "DESC")->paginate(10);
 
-        return response()->json($posts, 200);
+            if ($acceptHeader === 'application/json') {
+                return response()->json($posts->items('data'), 200);
+            } else {
+                $xml = new \SimpleXMLElement('<posts/>');
+                foreach ($posts->items('data') as $item) {
+                    $xmlItem = $xml->addChild('post');
+
+                    $xmlItem->addChild('id', $item->id);
+                    $xmlItem->addChild('title', $item->title);
+                    $xmlItem->addChild('status', $item->status);
+                    $xmlItem->addChild('content', $item->content);
+                    $xmlItem->addChild('user_id', $item->user_id);
+                    $xmlItem->addChild('created_at', $item->created_at);
+                    $xmlItem->addChild('updated_at', $item->updated_at);
+                }
+                return $xml->asXML();
+            }
+        } else {
+            return response('Not Acceptable!', 406);
+        }
     }
 
     public function store(Request $request)
-    {
-        $post = Post::create($request->all());
-        return response()->json($post, 200);
+{
+    $acceptHeader = $request->header('Accept');
+
+    if ($acceptHeader === 'application/json' || $acceptHeader === 'application/xml') {
+
+        $contentTypeHeader = $request->header('Content-Type');
+
+        if ($contentTypeHeader === 'application/json') {
+            // Manual validation
+            $validator = app('validator')->make($request->all(), [
+                'title' => 'required',
+                // tambahkan validasi untuk kolom-kolom lainnya
+            ]);
+
+            if ($validator->fails()) {
+                // Jika validasi gagal, kirim respon error
+                return response()->json(['error' => $validator->errors()], 422);
+            }
+
+            // Jika validasi berhasil, lanjutkan dengan membuat post
+            $input = $request->all();
+            $post = Post::create($input);
+
+            return response()->json($post, 200);
+        } else {
+            return response('Unsupported Media Type', 415);
+        }
+    } else {
+        return response('Not Acceptable!', 406);
     }
+}
+
 
     public function show($id)
     {
